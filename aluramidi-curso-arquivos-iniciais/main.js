23d2c90e5929dae765444bf50dbf8cb56adf8ffe
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-03 15:10:02 -0400

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,6 @@
-//aqui está sendo mapeado a tecla
 const teclaPom = document.querySelector('.tecla_pom');
 const teclaClap = document.querySelector('.tecla_clap');
 
-//aqui está sendo adicionado um evento de click ao button, que assim que for clicado, deverá reproduzir o som selecionado
 teclaPom.addEventListener("click", function() {
     const somTeclaPom = document.querySelector('#som_tecla_pom').play();
 });
