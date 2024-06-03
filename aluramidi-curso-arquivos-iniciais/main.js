406628a61a8bb465e5552465e56717ac071f9c09
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-03 15:09:12 -0400

    mapeando duas teclas e reproduzindo os som usando addEventListener

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,7 +1,12 @@
-//aqui está sendo mapeado a tecla_pom 
-let teclaPom = document.querySelector('.tecla_pom');
+//aqui está sendo mapeado a tecla
+const teclaPom = document.querySelector('.tecla_pom');
+const teclaClap = document.querySelector('.tecla_clap');
 
-//aqui está feito adicionado um evento de click ao button tecla_pom, que assim que for clicado, deverá reproduzir o som selecionado
+//aqui está sendo adicionado um evento de click ao button, que assim que for clicado, deverá reproduzir o som selecionado
 teclaPom.addEventListener("click", function() {
-    let somTeclaPom = document.querySelector('#som_tecla_pom').play();
+    const somTeclaPom = document.querySelector('#som_tecla_pom').play();
+});
+
+teclaClap.addEventListener("click", function() {
+    const somTeclaClap = document.querySelector('#som_tecla_clap').play();
 });
