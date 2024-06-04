e9ed1ef41dab089c92417ba953e1bf5092e7b7d3
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-03 21:53:50 -0400

    adicionando condição para conferir se foi clicado com 'Space' ou 'Enter'

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -15,8 +15,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     };
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
     };
 
     tecla.onkeyup = function () {
