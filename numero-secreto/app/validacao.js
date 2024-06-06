b22935a7f5ec1378a8b19e7f4c2d6ec6a5a4d94f
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-06 11:31:58 -0400

    s

---
+++ numero-secreto/app/validacao.js
@@ -0,0 +1,39 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if(chuteForInvalido(numero)) {
+        elementoChute.innerHTML += "<div>Valor invalido</div>";
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor Invalido: Fale um numero entre ${menorValor} e ${maiorValor}</div>
+        `;
+        return;
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Voce acertou!</h2>
+        <h3>O numero secreto era ${numeroSecreto}</h3>
+        `;
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O numero secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `;
+    } else {
+        elementoChute.innerHTML += `
+        <div>O numero secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `;
+    }
+};
+
+
+function chuteForInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
