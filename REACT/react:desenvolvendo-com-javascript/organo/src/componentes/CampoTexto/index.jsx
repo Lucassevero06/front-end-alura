f59f31b59f02b601d5d9faf565bdb9617783c8dd
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-09 10:29:07 -0400

    Update index.jsx

--- REACT/react:desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.jsx
+++ REACT/react:desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.jsx
@@ -4,10 +4,24 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
 
+    let valor = '';
+
+    const aoDigitado = (e) => {
+        valor = e.target.value;
+        console.log(valor);
+    }
+
     return (
         <div className='campo-texto'>
-            <label >{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada} />
+            <label>
+                {props.label}
+            </label>
+            <input
+                value={valor}
+                onChange={aoDigitado} 
+                required={props.obrigatorio} 
+                placeholder={placeholderModificada} 
+            />
         </div>
     )
 }
