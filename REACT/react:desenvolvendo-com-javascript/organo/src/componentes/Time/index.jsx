85ef4380b377506f11f56fef68a94d29793bf0e9
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-11 08:00:14 -0400

    estudando mais a fundo de como funciona um componente react

--- REACT/react:desenvolvendo-com-javascript/organo/src/componentes/Time/index.jsx
+++ REACT/react:desenvolvendo-com-javascript/organo/src/componentes/Time/index.jsx
@@ -4,11 +4,15 @@ import './Time.css'
 import Colaborador from '../Colaborador';
 
 const Time = (props) => {
+
+    function
+
     return (
         // verificando se o array colaboradores tem pelo menos um colaborador
         props.colaboradores.length > 0 && 
         // se for maior que 0, renderiza o time
         <section
+
             className='time'
             style={{
                 backgroundColor: props.corSecundaria
@@ -18,13 +22,18 @@ const Time = (props) => {
                     {props.nome}
                 </h3>
                 <div className='colaboradores'>
-                    {props.colaboradores.map(colaborador => <Colaborador
-                    corDeFundo={props.corPrimaria}
-                    key={colaborador.nome} 
-                    nome={colaborador.nome} 
-                    cargo={colaborador.cargo}
-                    imagem={colaborador.imagem}
-                    />)}
+                    {props.colaboradores.map(colaborador => 
+                        {
+                            return <Colaborador
+                                corDeFundo={props.corPrimaria}
+                                key={colaborador.nome} 
+                                nome={colaborador.nome} 
+                                cargo={colaborador.cargo}
+                                imagem={colaborador.imagem}
+                                aoDeletar={props.aoDeletar}
+                            />
+                        }  
+                    )}
                 </div>
                 
         </section>
